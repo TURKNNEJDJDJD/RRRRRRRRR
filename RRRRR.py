62af6ed2ea7ad227a22351af84ba38d85d0b05e6
TURKNNEJDJDJD <88967770+TURKNNEJDJDJD@users.noreply.github.com>
2021-08-15 14:40:41 +0300

    Update RRRRR.py

--- RRRRR.py
+++ RRRRR.py
@@ -700,5 +700,5 @@ if tlps == "5":
         else:
             b+=1    
             print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
-if tlps == "6":
+if tlps == "7":
         os.system('git clone https://github.com/TURKNNEJDJDJD/RRRRRRRRR ; cd RRRRRRRRR ;python2 RR1.py')
